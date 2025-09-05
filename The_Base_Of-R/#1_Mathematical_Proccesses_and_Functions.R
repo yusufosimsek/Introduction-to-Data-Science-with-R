05290ba5a66eb5a73381bc633682aad9a8158a0b
Yusuf Onur Şimşek <yusufsimsekk2@gmail.com>
2025-09-05 19:34:33 +0300

    Add files via upload

---
+++ The_Base_Of-R/#1_Mathematical_Proccesses_and_Functions.R
@@ -0,0 +1,34 @@
+                  # The Base Of R #
+
+### Base Mathematical Process ###
+## Addition makes "x + y" ##
+3+5
+##If I want to see in console result of that procces, I will touch  ctrl + enter ##
+## Subtriction makes "x - y" ##
+5-2
+## Multiplication makes "x * y" ##
+10*8
+## Division makes "x/y" ##
+8/5
+## Exponentiate makes "x**y" or "x^y" ##
+10**2
+5^5
+## Unremainder division makes "x %% y" ##
+10 %/% 3
+
+## Remainder fins "x %% y" ##
+10 %% 3
+
+
+            #### Functions #######
+## Funcitons divide two types: 
+## 1) Prepared Functions
+## 2) Functions that Our Write
+## Functions have ()
+
+## Function Sapmles:
+
+print(5)
+print ("hello world")
+
+
